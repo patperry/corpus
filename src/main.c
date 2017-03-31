d9daa8b97682ec695ee2c8cd6eaa44c64f8b9b69
Patrick Perry <patperry@gmail.com>
2017-03-31 16:15:40 -0400

    start corpus tool

--- src/main.c
+++ src/main.c
@@ -15,12 +15,64 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROGRAM_NAME	"corpus"
+#define PROGRAM_VERSION	"0.1"
+
+void usage(int status)
+{
+	printf("\
+Usage:\t%s [options] <command> [<args>]\n\
+Options:\n\
+\t-h\tPrints the help synopsis.\n\
+\t-v\tPrints the version number.\n\
+\n\
+Commands:\n\
+\tscan\tDetermine the schema of a data file\n\
+", PROGRAM_NAME);
+
+	exit(status);
+}
+
+
+void version()
+{
+	printf("%s version %s\n", PROGRAM_NAME, PROGRAM_VERSION);
+	exit(EXIT_SUCCESS);
+}
 
 
 int main(int argc, char * const argv[])
 {
-	printf("hello, world!\n");
-	(void)argc;
-	(void)argv;
+	int help = 0;
+	int ch;
+
+	while ((ch = getopt(argc, argv, "hv")) != -1) {
+		switch (ch) {
+		case 'h':
+			help = 1;
+			break;
+		case 'v':
+			version();
+			break;
+		default:
+			usage(EXIT_FAILURE);
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0) {
+		usage(EXIT_FAILURE);
+	} else if (strcmp(argv[0], "scan") != 0) {
+		fprintf(stderr, "Unrecognized command '%s'\n",
+			argv[0]);
+		usage(EXIT_FAILURE);
+	}
+
 	return 0;
 }

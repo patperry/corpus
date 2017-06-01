19020286869a21db029452c22845039622b3e67a
Patrick Perry <patperry@gmail.com>
2017-06-01 15:12:57 -0400

    fix copyright year

--- tests/check_tree.c
+++ tests/check_tree.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Patrick O. Perry.
+ * Copyright 2017 Patrick O. Perry.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
